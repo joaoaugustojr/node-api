8a6801f2710e14b3dbe12495c025f379425d53c4
João Augusto <50805318+joaoaugustojr@users.noreply.github.com>
2022-08-20 16:08:14 -0300

    test: add SignUpController test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,18 @@
-describe('SignUp Controller', () => {
-  test('should first', () => {
+import { SignUpController } from './signup'
 
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'john_doe',
+        email: 'john_doe@no-email.com',
+        password: 'foo_password',
+        passwordConfirmation: 'foo_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
   })
 })
