2ee628d0e9251c3ad0cfdedeadacd3962f46d1df
João Augusto <50805318+joaoaugustojr@users.noreply.github.com>
2022-08-20 16:19:28 -0300

    feat: ensure SignUpController tests and returns

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,7 +5,6 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'john_doe',
         email: 'john_doe@no-email.com',
         password: 'foo_password',
         passwordConfirmation: 'foo_password'
@@ -14,5 +13,21 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'john_doe',
+        password: 'foo_password',
+        passwordConfirmation: 'foo_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
